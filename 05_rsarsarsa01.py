62be426c297eff9ac1ad4689f9a3ff0b32fd4df2
Palash A <palasha.@Palashs-MacBook-Pro.local>
2019-01-21 11:54:05 +0530

    aesthetic

--- 05_rsarsarsa01.py
+++ 05_rsarsarsa01.py
@@ -3,23 +3,19 @@
 import os
 from helper import *
 
-def encrypt_list(lst,e,n):
+def modpow_list(lst,e,n):
   return [pow(x,e,n) for x in lst]
 
-def decrypt_list(lst,d,n):
-  return [pow(x,d,n) for x in lst]
-
-def encrypt(plain,e,n):
-  cipher = ''
-  for x in plain:
-    cipher += chr(pow(ord(x),e,n))
-  return cipher
-
-def decrypt(cipher,d,n):
-  plain = ''
-  for x in cipher:
-    plain += chr(pow(ord(x),d,n))
-  return plain
+# Sample cases
+SAMPLE = ['MYNAMEIS','WHAT','ATTACKATDAWN']
+P,Q,E,D = (7,11,13,37)
+for sample in SAMPLE:
+  alphabets = [ord(x)-64 for x in sample]
+  encrypted = modpow_list(alphabets,E,P*Q)
+  decrypted = modpow_list(encrypted,D,P*Q)
+  print(len(sample))
+  print(''.join(str(x)+' ' for x in encrypted).strip())
+  print(''.join(str(64+x) for x in decrypted))
 
 fnum = str(input("Enter filenum: "))
 fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,'05'),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
@@ -33,55 +29,23 @@ for i in range(t):
   # make keys
   while True:
     p,q = getRandomPrime(2,1,10**3)
-    if p!=q: break
-  phi = (p-1)*(q-1)
-  while True:
+    n = p*q
+    phi = (p-1)*(q-1)
     e = getRandomPrime(1,2,min(phi,10**3))
     g,d,_ = xgcd(e,phi)
     d %= phi
-    if g==1: break
-  n = p*q
-  print(g,p,q,e,d,n)
-  print(e*d%phi)
-
-  # make plaintext
+    if p!=q and g==1: break
   l = getRandom(1,2,10**2)
-  alph_list = list(getRandom(l,1,26))
-  plaintext = ''
-  for x in alph_list: plaintext += chr(64+x)
-
-  encrypted = encrypt(plaintext,e,n)
-  decrypted = decrypt(encrypted,d,n)
-
-  enc_list = encrypt_list(alph_list,e,n)
-  dec_list = decrypt_list(enc_list,d,n)
-
-  dec = ''
-  for x in dec_list: dec += chr(64+x)
-
-
-  if alph_list != dec_list: raise Exception('FALSE NIBBA!')
-
-  line = str(l) + '\n'
-  for x in enc_list: line+=str(x)+' '
-  line += '\n' + str(p) + ' ' +  str(n) + ' ' + str(e)
+  alphabets = list(getRandom(l,1,26))
+  encrypted = modpow_list(alph_list,e,n)
+  decrypted = modpow_list(enc_list,d,n)
+  plaintext = ''.join(chr(64+x) for x in decrypted)
+  # File write
+  line = str(l) + '\n' + ''.join(str(x)+' ' for x in encrypted).strip() + '\n' + str(p) + ' ' +  str(n) + ' ' + str(e)
   out = plaintext
   if i+1<t:
     line += '\n'
     out += '\n'
   fin.write(line)
   fout.write(out)
-
-#Sample cases
-# sample = ['MYNAMEIS','WHAT','ATTACKATDAWN']
-# P,Q,E,D = (7,11,13,37)
-# for s in sample:
-#   lst = [ord(x)-64 for x in s]
-#   enc_list = encrypt_list(lst,E,P*Q)
-#   dec_list = decrypt_list(enc_list,D,P*Q)
-#   dec = ''
-#   for x in dec_list: dec+=chr(64+x)
-#   print(enc_list)
-#   print(dec)
-
 print(datetime.datetime.now()-START)
