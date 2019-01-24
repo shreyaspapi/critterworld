cdec4b31df1ba2449135e067a3635aef3b9f3bb7
Palash A <palasha.@Palashs-MacBook-Pro.local>
2019-01-24 06:12:44 +0530

    URL and directory location 50%

---
+++ checker/plagiarism.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Check for perl
+# Get perl bin
+# run perl mossnet.pl [-l] [-b] filename
+
+import os, sys
+import datetime
+from helper import SUBMISSIONS_DIR
+
+# Get file submissions
+
+
---
+++ checker/scraper.py
@@ -0,0 +1,5 @@
+# Get (accepted) submissions AND leaderboard (w/ time and score)
+import requests
+from helper import SUBMISSIONS_DIR
+
+# Save to SUBMISSIONS_DIR
