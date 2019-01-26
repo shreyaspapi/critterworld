9532113b0e0cb2fbf0416469177f100d6bfd5966
Palash A <palash.agarwal.96@gmail.com>
2019-01-26 19:54:42 +0530

    week04/05_cipher solution; fixed dir problem in templates

--- week04/src/05_cipher.py
+++ week04/src/05_cipher.py
@@ -2,11 +2,14 @@ import os
 import datetime
 import helper
 from helper import TEST_DIR, INPUT_DIR, OUTPUT_DIR, INPUT_PRE, OUTPUT_PRE, FILE_EXT
-PROBLEM_NUM = '' # Change according to problem number in contest
+PROBLEM_NUM = '05' # Change according to problem number in contest
 
 # Solution
-def solution():
-  pass
+def encrypt(plaintext,n):
+  ciphertext = ''
+  for i in range(n):
+    ciphertext += ''.join([plaintext[x] for x in range(i,len(plaintext),n)])
+  return ciphertext
 
 # Sample cases
 SAMPLE = []
@@ -14,12 +17,16 @@ SAMPLE = []
 
 # Build tests
 fnum = input("Enter testfile num: ")
+fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
+fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
 t = int(input("Test cases: "))
+fin.write(str(t)+'\n')
+
 TIME = []
 for i in range(t):
   # Inputs
-  n = helper.get_random(1,1,100)
-  length = helper.get_random(1,1,n*n)
+  n = helper.get_random(1,2,100)
+  length = helper.get_random(1,2,n*n)
   underscores = n*n - length
   alphabets = list(helper.get_random(length,1,26))
   string = [chr(64+x) for x in alphabets]
@@ -28,17 +35,15 @@ for i in range(t):
   message = ''.join(string)
   # Result
   START = datetime.datetime.now()
-  res = solution()
+  res = encrypt(message,n)
   END = datetime.datetime.now()
-  TIME.append(START-END)
+  TIME.append(END-START)
   # File write
   input_line = str(n) + '\n' + message
-  output_line = ''
+  output_line = res
   if i+1<t:
     input_line += '\n'
     output_line += '\n'
-  fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
-  fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
   fin.write(input_line)
   fout.write(output_line)
-print("Time:",sum(TIME))
+print("Time:",sum(TIME,datetime.timedelta(0)))
