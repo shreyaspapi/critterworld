acec7d6ce17b68441d035f0486b64723b3c6de8f
Palash A <palash.agarwal.96@gmail.com>
2019-01-26 16:01:12 +0530

    added random function to helper

--- templates/helper.py
+++ templates/helper.py
@@ -12,3 +12,14 @@ OUTPUT_DIR = 'output'
 OUTPUT_PRE = 'output'
 
 # functions
+
+''' 
+Get n random numbers in range [start,end]
+Returns single number (n==1) or a tuple (n>1)
+'''
+def getRandom(n,start,end):
+  import random
+  if n == 1: return random.randint(start,end)
+  res = ()
+  for _ in range(n): res += (random.randint(start,end),)
+  return res
