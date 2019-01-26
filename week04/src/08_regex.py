f6e3838f2c3871a400b85a5e9ab043e07782c498
Palash A <palash.agarwal.96@gmail.com>
2019-01-26 19:55:57 +0530

    updated all week04 problems to reflect template/problem

--- week04/src/08_regex.py
+++ week04/src/08_regex.py
@@ -14,7 +14,11 @@ SAMPLE = []
 
 # Build tests
 fnum = input("Enter testfile num: ")
+fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
+fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
 t = int(input("Test cases: "))
+fin.write(str(t)+'\n')
+
 TIME = []
 for i in range(t):
   # Inputs
@@ -29,8 +33,6 @@ for i in range(t):
   if i+1<t:
     input_line += '\n'
     output_line += '\n'
-  fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
-  fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
   fin.write(input_line)
   fout.write(output_line)
-print("Time:",sum(TIME))
+print("Time:",sum(TIME,datetime.timedelta(0)))
