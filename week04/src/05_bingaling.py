f1ad12c53a0f2e82321fd41b6b03137453cae73c
shreyaspapi <spapinwar@gmail.com>
2019-01-27 03:26:41 +0530

    Probably right 05_bingaling.py

--- week04/src/05_bingaling.py
+++ week04/src/05_bingaling.py
@@ -14,49 +14,50 @@ def first_last(all_plates):
 
 # Solution
 def solution(listOfPlates):
-  first, last = firstLast(listOfPlates)
-  last_temp = last[:]
-  ans = []
-  flag = 0
+  first, last = first_last(listOfPlates)
+  ans = [first[0], last[0]]
+  first, last = first[1:], last[1:]
   for i in range(len(first)):
-    temp = 0
-    for j in range(len(last)):
-      if last[i] == first[j]:
-          first[j] == "{}{}".format(i j)
-          temp = 1
-          break
-    if temp == 0:
-      flag += 1
-  if flag < 2:
-    for i in first:
-      if 
-  return -1
+    if ans[-1] in first:
+      index = first.index(ans[-1])
+      ans.append(first[index])
+      ans.append(last[index])
+      first[index], last[index] = "", ""
+    elif ans[0] in last:
+      index = last.index(ans[0])
+      ans.insert(0, last[index])
+      ans.insert(0, first[index])
+      first[index], last[index] = "", ""
+  for i in first:
+    if i != "":
+      return -1
+  return ans
 
-# Sample cases
-SAMPLE = []
+# # Sample cases
+# SAMPLE = []
 
 
-# Build tests
-fnum = input("Enter testfile num: ")
-fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
-fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
-t = int(input("Test cases: "))
-fin.write(str(t)+'\n')
+# # Build tests
+# fnum = input("Enter testfile num: ")
+# fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
+# fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
+# t = int(input("Test cases: "))
+# fin.write(str(t)+'\n')
 
-TIME = []
-for i in range(t):
-  # Inputs
-  # Result
-  START = datetime.datetime.now()
-  res = solution()
-  END = datetime.datetime.now()
-  TIME.append(START-END)
-  # File write
-  input_line = ''
-  output_line = ''
-  if i+1<t:
-    input_line += '\n'
-    output_line += '\n'
-  fin.write(input_line)
-  fout.write(output_line)
-print("Time:",sum(TIME,datetime.timedelta(0)))
+# TIME = []
+# for i in range(t):
+#   # Inputs
+#   # Result
+#   START = datetime.datetime.now()
+#   res = solution()
+#   END = datetime.datetime.now()
+#   TIME.append(START-END)
+#   # File write
+#   input_line = ''
+#   output_line = ''
+#   if i+1<t:
+#     input_line += '\n'
+#     output_line += '\n'
+#   fin.write(input_line)
+#   fout.write(output_line)
+# print("Time:",sum(TIME,datetime.timedelta(0)))
