b1925703c6b116134d5b9e05d54b30a6777e1f98
Palash A <palash.agarwal.96@gmail.com>
2019-01-26 21:25:56 +0530

    generating inputs for koro; accepted shreyas's change on surupam

--- week04/src/01_koro.py
+++ week04/src/01_koro.py
@@ -2,10 +2,10 @@ import os
 import datetime
 import helper
 from helper import TEST_DIR, INPUT_DIR, OUTPUT_DIR, INPUT_PRE, OUTPUT_PRE, FILE_EXT
-PROBLEM_NUM = '' # Change according to problem number in contest
+PROBLEM_NUM = '01' # Change according to problem number in contest
 
 # Solution
-def solution():
+def num_palindromes(string,variations):
   pass
 
 # Sample cases
@@ -22,14 +22,19 @@ fin.write(str(t)+'\n')
 TIME = []
 for i in range(t):
   # Inputs
+  length = helper.get_random(1,10,1000)
+  n = helper.get_random(1,1,min(100,length//2))
+  print(i,n,length)
+  alphabets = list(helper.get_random(length,1,26))
+  string = ''.join([chr(96+x) for x in alphabets])
   # Result
   START = datetime.datetime.now()
-  res = solution()
+  res = num_palindromes(string,n)
   END = datetime.datetime.now()
-  TIME.append(START-END)
+  TIME.append(END-START)
   # File write
-  input_line = ''
-  output_line = ''
+  input_line = string + '\n' + str(n)
+  output_line = str(res)
   if i+1<t:
     input_line += '\n'
     output_line += '\n'
