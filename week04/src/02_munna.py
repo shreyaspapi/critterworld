1745acdcca1c977564c196ab40659f1bbf6b508e
Palash A <palash.agarwal.96@gmail.com>
2019-01-26 22:36:22 +0530

    munna changed to reflect template/program

--- week04/src/02_munna.py
+++ week04/src/02_munna.py
@@ -22,7 +22,11 @@ SAMPLE = []
 
 # Build tests
 fnum = input("Enter testfile num: ")
+fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
+fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
 t = int(input("Test cases: "))
+fin.write(str(t)+'\n')
+
 TIME = []
 for i in range(t):
   # Inputs
@@ -30,15 +34,13 @@ for i in range(t):
   START = datetime.datetime.now()
   res = solution()
   END = datetime.datetime.now()
-  TIME.append(START-END)
+  TIME.append(END-START)
   # File write
   input_line = ''
   output_line = ''
   if i+1<t:
     input_line += '\n'
     output_line += '\n'
-  fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
-  fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
   fin.write(input_line)
   fout.write(output_line)
-print("Time:",sum(TIME))
+print("Time:",sum(TIME,datetime.timedelta(0)))
