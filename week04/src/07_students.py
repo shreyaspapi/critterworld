8a3a254c7fe788da48682509d3823d364c69105e
shreyaspapi <spapinwar@gmail.com>
2019-01-28 01:29:04 +0530

    Adding solution of 07_students.py

--- week04/src/07_students.py
+++ week04/src/07_students.py
@@ -36,27 +36,28 @@ def solution(submitted_answer, correct_answer):
 SAMPLE = []
 
 
-# Build tests
-fnum = input("Enter testfile num: ")
-fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
-fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
-t = int(input("Test cases: "))
-fin.write(str(t)+'\n')
 
-TIME = []
-for i in range(t):
-  # Inputs
-  # Result
-  START = datetime.datetime.now()
-  res = solution()
-  END = datetime.datetime.now()
-  TIME.append(START-END)
-  # File write
-  input_line = ''
-  output_line = ''
-  if i+1<t:
-    input_line += '\n'
-    output_line += '\n'
-  fin.write(input_line)
-  fout.write(output_line)
-print("Time:",sum(TIME,datetime.timedelta(0)))
+# # Build tests
+# fnum = input("Enter testfile num: ")
+# fin = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),INPUT_DIR),INPUT_PRE+fnum+FILE_EXT),'w')
+# fout = open(os.path.join(os.path.join(os.path.join(TEST_DIR,PROBLEM_NUM),OUTPUT_DIR),OUTPUT_PRE+fnum+FILE_EXT),'w')
+# t = int(input("Test cases: "))
+# fin.write(str(t)+'\n')
+
+# TIME = []
+# for i in range(t):
+#   # Inputs
+#   # Result
+#   START = datetime.datetime.now()
+#   res = solution()
+#   END = datetime.datetime.now()
+#   TIME.append(START-END)
+#   # File write
+#   input_line = ''
+#   output_line = ''
+#   if i+1<t:
+#     input_line += '\n'
+#     output_line += '\n'
+#   fin.write(input_line)
+#   fout.write(output_line)
+# print("Time:",sum(TIME,datetime.timedelta(0)))
