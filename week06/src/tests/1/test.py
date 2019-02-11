10e391b25980df7e5d278a64c45d7a3fb5adc873
shreyaspapi <spapinwar@gmail.com>
2019-02-12 02:31:37 +0530

    adding input for 1 and 2

---
+++ week06/src/tests/1/test.py
@@ -0,0 +1,19 @@
+import random
+
+for i in range(1, 6):
+    doc = open("{}.txt".format(i), "w")
+
+    t = random.randint(1, 10)
+    
+    doc.write(str(t) + "\n")
+    for _ in range(t):
+        n = random.randint(1, 20000)
+        doc.write(str(n) + "\n")
+        for _ in range(n-1):
+            x = random.randint(1, n)
+            y = random.randint(1, n)
+            while x == y:
+                y = random.randint(1, n)
+            z = random.randint(1, 10**9)
+            doc.write(str(x) + " " + str(y) + " " + str(z) + "\n")
+            
---
+++ week06/src/tests/2/test.py
@@ -0,0 +1,19 @@
+import random
+
+for i in range(1, 6):
+    doc = open("{}.txt".format(i), "w")
+    
+    t = random.randint(1, 5)
+    doc.write(str(t)+"\n")
+    n, q = random.randint(1, 10 ** 5), random.randint(1, 10**5)
+    doc.write(str(n) + " " + str(q) + "\n")
+    for _ in range(n-1):
+        u, v = random.randint(1, n), random.randint(1, n)
+        while n == v:
+            v = random.randint(1, n)
+        doc.write(str(u) + " " + str(v) + "\n")
+    for _ in range(q):
+        x, y = random.randint(1, n), random.randint(1, n)
+        while x == y:
+            y = random.randint(1, n)
+        doc.write(str(x) + " " + str(y) + "\n")
