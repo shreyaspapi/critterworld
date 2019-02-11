cd72474a7b11d97a956d673970721012d0cbdcab
shreyaspapi <spapinwar@gmail.com>
2019-02-12 01:54:17 +0530

    adding test cases for q5 and q4 week06

---
+++ week06/src/tests/5/test_alarsh.py
@@ -0,0 +1,26 @@
+import random
+
+for i in range(1, 6):
+    doc = open("{}.txt".format(i), "w")
+
+    t = random.randint(1, 10)
+    doc.write(str(t) + "\n")
+    for _ in range(t):
+        n = random.randint(2, 10**6)
+        m = random.randint(2, n)
+        doc.write(str(n) + " " + str(m) + "\n")
+
+        for i in range(n):
+            u, v = random.randint(1, n), random.randint(1, n)
+            doc.write(str(u) + " " + str(v) + "\n")
+
+        m_space = random.sample(range(1, n), m)
+        str_m = ""
+        for i in m_space:
+            str_m += (str(i) + " ")
+
+        doc.write(str_m + "\n")
+
+    doc.close()
+    
+
